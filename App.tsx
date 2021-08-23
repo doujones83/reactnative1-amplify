3f5e3036157dd0cc60802fbf714f84db2d4c577d
William Boswell <doujones@Williams-MacBook-Air.local>
2021-08-23 12:22:57 -0400

    third commit

--- App.tsx
+++ App.tsx
@@ -10,17 +10,9 @@ Amplify.configure(awsExports)
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
